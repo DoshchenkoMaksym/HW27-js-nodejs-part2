752a5bb05892205be93b021ed042a95039f37311
Maksym Doshchenko <doschenko54@gmail.com>
2021-10-04 11:58:42 +0300

    init

---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var url = require('url')
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(function(req, res, next) {
+    if (req.url == '/home') {
+      res.end("Home");
+    } else {
+      next();
+    }
+  });
+  
+  app.use(function(req, res, next) {
+    if (!CheckAccess(req)) {
+      res.end("Access denied");
+      return
+    } else {
+    res.end("Access approved");
+    }
+  });
+
+  function CheckAccess(req) {
+      console.log(url.parse(req.url, true).query.secret)
+      return url.parse(req.url, true).query.secret === 'true';
+  }
+module.exports = app;
